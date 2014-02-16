bfcb932fa910e7dbca73a5e50d8482f650ed7e5c
Dane Springmeyer <dane@dbsgeo.com>
2014-02-15 17:14:59 -0800

    break out geometry encoder

--- src/vector_tile_backend_pbf.hpp
+++ src/vector_tile_backend_pbf.hpp
@@ -7,6 +7,7 @@
 
 // vector tile
 #include "vector_tile.pb.h"
+#include "vector_tile_geometry_encoder.hpp"
 
 // boost
 #include <boost/unordered_map.hpp>
@@ -155,110 +156,28 @@ namespace mapnik { namespace vector {
             current_layer_->set_extent(256 * path_multiplier_);
         }
 
-        void stop_tile_layer()
+        inline void stop_tile_layer()
         {
             //std::cerr << "stop_tile_layer()" << std::endl;
         }
 
         template <typename T>
-        unsigned add_path(T & path, unsigned tolerance, mapnik::eGeomType type)
+        inline unsigned add_path(T & path, unsigned tolerance, mapnik::eGeomType type)
         {
-            unsigned count = 0;
-
             if (current_feature_)
             {
-                path.rewind(0);
-                current_feature_->set_type(mapnik::vector::tile_GeomType(type));
-
-                vertex2d vtx(vertex2d::no_init);
-                int cmd = -1;
-                int cmd_idx = -1;
-                const int cmd_bits = 3;
-                unsigned length = 0;
-                bool skipped_last = false;
-                int32_t cur_x = 0;
-                int32_t cur_y = 0;
-
-                // See vector_tile.proto for a description of how vertex command
-                // encoding works.
-                while ((vtx.cmd = path.vertex(&vtx.x, &vtx.y)) != SEG_END)
-                {
-                    if (static_cast<int>(vtx.cmd) != cmd)
-                    {
-                        if (cmd_idx >= 0)
-                        {
-                            // Encode the previous length/command value.
-                            current_feature_->set_geometry(cmd_idx, (length << cmd_bits) | (cmd & ((1 << cmd_bits) - 1)));
-                        }
-                        cmd = static_cast<int>(vtx.cmd);
-                        length = 0;
-                        cmd_idx = current_feature_->geometry_size();
-                        current_feature_->add_geometry(0); // placeholder added in first pass
-                    }
-
-                    if (cmd == SEG_MOVETO || cmd == SEG_LINETO)
-                    {
-                        // Compute delta to the previous coordinate.
-                        cur_x = static_cast<int32_t>(std::floor((vtx.x * path_multiplier_) + 0.5));
-                        cur_y = static_cast<int32_t>(std::floor((vtx.y * path_multiplier_) + 0.5));
-                        int32_t dx = cur_x - x_;
-                        int32_t dy = cur_y - y_;
-
-                        // Keep all move_to commands, but omit other movements that are
-                        // not >= the tolerance threshold and should be considered no-ops.
-                        if ( cmd == SEG_MOVETO ||
-                             (static_cast<unsigned>(std::abs(dx)) >= tolerance) ||
-                             (static_cast<unsigned>(std::abs(dy)) >= tolerance) ||
-                             length == 0
-                           )
-                        {
-                            // Manual zigzag encoding.
-                            current_feature_->add_geometry((dx << 1) ^ (dx >> 31));
-                            current_feature_->add_geometry((dy << 1) ^ (dy >> 31));
-                            x_ = cur_x;
-                            y_ = cur_y;
-                            skipped_last = false;
-                            ++length;
-                        }
-                        else
-                        {
-                            skipped_last = true;
-                        }
-                    }
-                    else if (cmd == SEG_CLOSE)
-                    {
-                        ++length;
-                    }
-                    else
-                    {
-                        std::stringstream msg;
-                        msg << "Unknown command type (backend_pbf): "
-                            << cmd;
-                        throw std::runtime_error(msg.str());
-                    }
-
-                    ++count;
-                }
-
-                if (skipped_last && length > 1)
-                {
-                    int32_t dx = cur_x - x_;
-                    int32_t dy = cur_y - y_;
-                    current_feature_->add_geometry((dx << 1) ^ (dx >> 31));
-                    current_feature_->add_geometry((dy << 1) ^ (dy >> 31));
-                    ++length;
-                }
-
-                // Update the last length/command value.
-                if (cmd_idx >= 0)
-                {
-                    current_feature_->set_geometry(cmd_idx, (length << cmd_bits) | (cmd & ((1 << cmd_bits) - 1)));
-                }
+                return encode_geometry(path,
+                                       static_cast<tile_GeomType>(type),
+                                       *current_feature_,
+                                       x_,
+                                       y_,
+                                       tolerance,
+                                       path_multiplier_);
             }
-            return count;
+            return 0;
         }
     };
 
-    }} // end ns
+}} // end ns
 
 #endif // __MAPNIK_VECTOR_TILE_BACKEND_PBF_H__
---
+++ src/vector_tile_geometry_encoder.hpp
@@ -0,0 +1,112 @@
+#ifndef __MAPNIK_VECTOR_TILE_GEOMETRY_ENCODER_H__
+#define __MAPNIK_VECTOR_TILE_GEOMETRY_ENCODER_H__
+
+// vector tile
+#include "vector_tile.pb.h"
+#include <mapnik/vertex.hpp>
+
+namespace mapnik { namespace vector {
+
+        template <typename T>
+        unsigned encode_geometry(T & path,
+                                 tile_GeomType type,
+                                 tile_feature & current_feature,
+                                 int32_t & x_,
+                                 int32_t & y_,
+                                 unsigned tolerance,
+                                 unsigned path_multiplier)
+        {
+            unsigned count = 0;
+            path.rewind(0);
+            current_feature.set_type(type);
+
+            vertex2d vtx(vertex2d::no_init);
+            int cmd = -1;
+            int cmd_idx = -1;
+            const int cmd_bits = 3;
+            unsigned length = 0;
+            bool skipped_last = false;
+            int32_t cur_x = 0;
+            int32_t cur_y = 0;
+
+            // See vector_tile.proto for a description of how vertex command
+            // encoding works.
+            while ((vtx.cmd = path.vertex(&vtx.x, &vtx.y)) != SEG_END)
+            {
+                if (static_cast<int>(vtx.cmd) != cmd)
+                {
+                    if (cmd_idx >= 0)
+                    {
+                        // Encode the previous length/command value.
+                        current_feature.set_geometry(cmd_idx, (length << cmd_bits) | (cmd & ((1 << cmd_bits) - 1)));
+                    }
+                    cmd = static_cast<int>(vtx.cmd);
+                    length = 0;
+                    cmd_idx = current_feature.geometry_size();
+                    current_feature.add_geometry(0); // placeholder added in first pass
+                }
+
+                if (cmd == SEG_MOVETO || cmd == SEG_LINETO)
+                {
+                    // Compute delta to the previous coordinate.
+                    cur_x = static_cast<int32_t>(std::floor((vtx.x * path_multiplier) + 0.5));
+                    cur_y = static_cast<int32_t>(std::floor((vtx.y * path_multiplier) + 0.5));
+                    int32_t dx = cur_x - x_;
+                    int32_t dy = cur_y - y_;
+
+                    // Keep all move_to commands, but omit other movements that are
+                    // not >= the tolerance threshold and should be considered no-ops.
+                    if ( cmd == SEG_MOVETO ||
+                         (static_cast<unsigned>(std::abs(dx)) >= tolerance) ||
+                         (static_cast<unsigned>(std::abs(dy)) >= tolerance) ||
+                         length == 0
+                       )
+                    {
+                        // Manual zigzag encoding.
+                        current_feature.add_geometry((dx << 1) ^ (dx >> 31));
+                        current_feature.add_geometry((dy << 1) ^ (dy >> 31));
+                        x_ = cur_x;
+                        y_ = cur_y;
+                        skipped_last = false;
+                        ++length;
+                    }
+                    else
+                    {
+                        skipped_last = true;
+                    }
+                }
+                else if (cmd == SEG_CLOSE)
+                {
+                    ++length;
+                }
+                else
+                {
+                    std::stringstream msg;
+                    msg << "Unknown command type (backend_pbf): "
+                        << cmd;
+                    throw std::runtime_error(msg.str());
+                }
+
+                ++count;
+            }
+
+            if (skipped_last && length > 1)
+            {
+                int32_t dx = cur_x - x_;
+                int32_t dy = cur_y - y_;
+                current_feature.add_geometry((dx << 1) ^ (dx >> 31));
+                current_feature.add_geometry((dy << 1) ^ (dy >> 31));
+                ++length;
+            }
+
+            // Update the last length/command value.
+            if (cmd_idx >= 0)
+            {
+                current_feature.set_geometry(cmd_idx, (length << cmd_bits) | (cmd & ((1 << cmd_bits) - 1)));
+            }
+            return count;
+        }
+
+}} // end ns
+
+#endif // __MAPNIK_VECTOR_TILE_GEOMETRY_ENCODER_H__
