e3bac2d34b2fd391a34a02c574b21d5fb4798560
artemp <artem@mapnik.org>
2015-04-01 15:06:47 +0200

    ensure we're returning the same object from multiple return paths and expect RVO

--- src/vector_tile_geometry_decoder.hpp
+++ src/vector_tile_geometry_decoder.hpp
@@ -16,10 +16,10 @@ public:
 
     enum command : uint8_t {
         end = 0,
-        move_to = 1,
-        line_to = 2,
-        close = 7
-    };
+            move_to = 1,
+            line_to = 2,
+            close = 7
+            };
 
     inline command next(double& rx, double& ry);
 
@@ -88,149 +88,158 @@ Geometry::command Geometry::next(double& rx, double& ry) {
 }
 
 inline mapnik::geometry::geometry decode_geometry(vector_tile::Tile_Feature const& f,
-                                           double tile_x, double tile_y,
-                                           double scale_x, double scale_y)
+                                                  double tile_x, double tile_y,
+                                                  double scale_x, double scale_y)
 {
-
     Geometry::command cmd;
     Geometry geoms(f,tile_x,tile_y,scale_x,scale_y);
     double x1, y1;
+    mapnik::geometry::geometry geom = mapnik::geometry::geometry_empty(); // output geometry
+
     switch (f.type())
     {
-        case vector_tile::Tile_GeomType_POINT:
+    case vector_tile::Tile_GeomType_POINT:
+    {
+        mapnik::geometry::multi_point mp;
+        while ((cmd = geoms.next(x1, y1)) != Geometry::end)
         {
-            mapnik::geometry::multi_point mp;
-            while ((cmd = geoms.next(x1, y1)) != Geometry::end) {
-                mp.emplace_back(mapnik::geometry::point(x1,y1));
-            }
-            std::size_t num_points = mp.size();
-            if (num_points == 1)
-            {
-                // return the single point
-                return mapnik::geometry::geometry(std::move(mp[0]));;
-            }
-            else if (num_points > 1)
-            {
-                // return multipoint
-                return mapnik::geometry::geometry(std::move(mp));;
-            }
-            break;
+            mp.emplace_back(mapnik::geometry::point(x1,y1));
         }
-        case vector_tile::Tile_GeomType_LINESTRING:
+        std::size_t num_points = mp.size();
+        if (num_points == 1)
         {
-            mapnik::geometry::multi_line_string mp;
-            mp.emplace_back();
-            bool first = true;
-            while ((cmd = geoms.next(x1, y1)) != Geometry::end) {
-                if (cmd == Geometry::move_to) {
-                    if (first)
-                    {
-                        first = false;
-                    }
-                    else
-                    {
-                        mp.emplace_back();
-                    }
-                }
-                mp.back().add_coord(x1,y1);
-            }
-            std::size_t num_lines = mp.size();
-            if (num_lines == 1)
-            {
-                // return the single line
-                auto itr = std::make_move_iterator(mp.begin());
-                return mapnik::geometry::geometry(std::move(*itr));
-            }
-            else if (num_lines > 1)
-            {
-                // return multiline
-                return mapnik::geometry::geometry(std::move(mp));
-            }
-            break;
+            // return the single point
+            geom = std::move(mp[0]);
+            return geom;
         }
-        case vector_tile::Tile_GeomType_POLYGON:
+        else if (num_points > 1)
         {
-            // TODO - support for multipolygons
-            //mapnik::geometry::multi_polygon;
-            mapnik::geometry::polygon poly;
-            std::vector<mapnik::geometry::linear_ring> rings;
-            rings.emplace_back();
-            double x2,y2;
-            bool first = true;
-            while ((cmd = geoms.next(x1, y1)) != Geometry::end)
-            {
-                if (cmd == Geometry::move_to)
-                {
-                    x2 = x1;
-                    y2 = y1;
-                    if (first)
-                    {
-                        first = false;
-                    }
-                    else
-                    {
-                        rings.emplace_back();
-                    }
-                }
-                else if (cmd == Geometry::close)
-                {
-                    rings.back().add_coord(x2,y2);
-                    continue;
-                }
-                rings.back().add_coord(x1,y1);
-            }
-
-            auto rings_itr = std::make_move_iterator(rings.begin());
-            auto rings_end = std::make_move_iterator(rings.end());
-            std::size_t num_rings = rings.size();
-            if (num_rings == 1)
-            {
-                // return the single polygon without interior rings
-                mapnik::geometry::polygon poly;
-                poly.set_exterior_ring(std::move(*rings_itr));
-                return mapnik::geometry::geometry(std::move(poly));
-            }
-
-            mapnik::geometry::multi_polygon multi_poly;
-            multi_poly.emplace_back();
-            first = true;
-            for (; rings_itr != rings_end; ++rings_itr)
+            // return multipoint
+            geom = std::move(mp);
+            return geom;
+        }
+        break;
+    }
+    case vector_tile::Tile_GeomType_LINESTRING:
+    {
+        mapnik::geometry::multi_line_string mp;
+        mp.emplace_back();
+        bool first = true;
+        while ((cmd = geoms.next(x1, y1)) != Geometry::end)
+        {
+            if (cmd == Geometry::move_to)
             {
                 if (first)
                 {
-                    // first ring always exterior
-                    multi_poly.back().set_exterior_ring(std::move(*rings_itr));
                     first = false;
-                }
-                else if (!mapnik::util::is_clockwise(*rings_itr)) // no-move --> const&
-                {
-                    multi_poly.emplace_back(); // start new polygon
-                    multi_poly.back().set_exterior_ring(std::move(*rings_itr));
                 }
                 else
                 {
-                    multi_poly.back().add_hole(std::move(*rings_itr));
+                    mp.emplace_back();
                 }
             }
-            if (multi_poly.size() == 1)
+            mp.back().add_coord(x1,y1);
+        }
+        std::size_t num_lines = mp.size();
+        if (num_lines == 1)
+        {
+            // return the single line
+            auto itr = std::make_move_iterator(mp.begin());
+            geom = std::move(*itr);
+            return geom;
+        }
+        else if (num_lines > 1)
+        {
+            // return multiline
+            geom = std::move(mp);
+            return geom;
+        }
+        break;
+    }
+    case vector_tile::Tile_GeomType_POLYGON:
+    {
+        mapnik::geometry::polygon poly;
+        std::vector<mapnik::geometry::linear_ring> rings;
+        rings.emplace_back();
+        double x2,y2;
+        bool first = true;
+        while ((cmd = geoms.next(x1, y1)) != Geometry::end)
+        {
+            if (cmd == Geometry::move_to)
             {
-                auto itr = std::make_move_iterator(multi_poly.begin());
-                return mapnik::geometry::geometry(std::move(*itr));
+                x2 = x1;
+                y2 = y1;
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    rings.emplace_back();
+                }
+            }
+            else if (cmd == Geometry::close)
+            {
+                rings.back().add_coord(x2,y2);
+                continue;
+            }
+            rings.back().add_coord(x1,y1);
+        }
+
+        auto rings_itr = std::make_move_iterator(rings.begin());
+        auto rings_end = std::make_move_iterator(rings.end());
+        std::size_t num_rings = rings.size();
+        if (num_rings == 1)
+        {
+            // return the single polygon without interior rings
+            mapnik::geometry::polygon poly;
+            poly.set_exterior_ring(std::move(*rings_itr));
+            geom = std::move(poly);
+            return geom;
+        }
+
+        mapnik::geometry::multi_polygon multi_poly;
+        multi_poly.emplace_back();
+        first = true;
+        for (; rings_itr != rings_end; ++rings_itr)
+        {
+            if (first)
+            {
+                // first ring always exterior
+                multi_poly.back().set_exterior_ring(std::move(*rings_itr));
+                first = false;
+            }
+            else if (!mapnik::util::is_clockwise(*rings_itr)) // no-move --> const&
+            {
+                multi_poly.emplace_back(); // start new polygon
+                multi_poly.back().set_exterior_ring(std::move(*rings_itr));
             }
             else
             {
-                return mapnik::geometry::geometry(std::move(multi_poly));
+                multi_poly.back().add_hole(std::move(*rings_itr));
             }
-            break;
         }
-        case vector_tile::Tile_GeomType_UNKNOWN:
-        default:
+        if (multi_poly.size() == 1)
         {
-            throw std::runtime_error("unhandled geometry type during decoding");
-            break;
+            auto itr = std::make_move_iterator(multi_poly.begin());
+            geom = std::move(*itr);
+            return geom;
         }
+        else
+        {
+            geom = std::move(multi_poly);
+            return geom;
+        }
+        break;
     }
-    return mapnik::geometry::geometry();
+    case vector_tile::Tile_GeomType_UNKNOWN:
+    default:
+    {
+        throw std::runtime_error("unhandled geometry type during decoding");
+        break;
+    }
+    }
+    return geom;
 }
 
 }} // end ns
