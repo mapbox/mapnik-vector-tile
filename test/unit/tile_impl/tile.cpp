fd2cc8b669f678c80ec86e681aaa6aa20872c9f6
jakepruitt <jake@mapbox.com>
2016-01-20 10:14:58 -0700

    Add more edge case constructions

--- test/unit/tile_impl/tile.cpp
+++ test/unit/tile_impl/tile.cpp
@@ -36,4 +36,39 @@ TEST_CASE("Vector tile base class")
         t = default_tile.get_tile();
         CHECK(t.layers_size() == 0);
     }
+
+    SECTION("construction with zero tile_size")
+    {
+        mapnik::vector_tile_impl::tile zero_size_tile(global_extent, 0);
+
+        CHECK(zero_size_tile.tile_size() == 0);
+        CHECK(std::abs(zero_size_tile.scale() - 40075016.6855780035) < 0.00001);
+        CHECK(zero_size_tile.get_buffered_extent() == global_extent);
+    }
+
+    SECTION("construction with negative tile_size")
+    {
+        mapnik::vector_tile_impl::tile negative_size_tile(global_extent, -1);
+
+        CHECK(negative_size_tile.tile_size() == 4294967295);
+        CHECK(std::abs(negative_size_tile.scale() - 0.0093306919) < 0.0000001);
+        CHECK(negative_size_tile.get_buffered_extent() == global_extent);
+    }
+
+    SECTION("construction with positive buffer size")
+    {
+        mapnik::vector_tile_impl::tile positive_buffer_tile(global_extent, 4096, 10);
+
+        mapnik::box2d<double> buffered_extent(-20135347.7389940246939659,-20135347.7389940246939659,20135347.7389940246939659,20135347.7389940246939659);
+        CHECK(positive_buffer_tile.get_buffered_extent() == buffered_extent);
+        CHECK(positive_buffer_tile.buffer_size() == 10);
+    }
+
+    SECTION("construction with very negative buffer size")
+    {
+        mapnik::vector_tile_impl::tile negative_buffer_tile(global_extent, 4096, -4000);
+        mapnik::box2d<double> buffered_extent(0.0, 0.0, 0.0, 0.0);
+        CHECK(negative_buffer_tile.get_buffered_extent() == buffered_extent);
+        CHECK(negative_buffer_tile.buffer_size() == -4000);
+    }
 }
