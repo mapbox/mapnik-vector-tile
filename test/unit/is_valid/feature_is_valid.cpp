764bd60b0f0e5a603b08806c91f0d9614c2a5c9b
jakepruitt <jake@mapbox.com>
2016-02-05 16:16:20 -0700

    Add more tests

--- test/unit/is_valid/feature_is_valid.cpp
+++ test/unit/is_valid/feature_is_valid.cpp
@@ -8,19 +8,92 @@
 
 #include "vector_tile.pb.h"
 
-
+typedef std::set<mapnik::vector_tile_impl::validity_error> error_set_T;
 
 TEST_CASE( "invalid empty feature" )
 {
     std::string buffer;
-    vector_tile::Tile_Feature value;
-    std::set<mapnik::vector_tile_impl::validity_error> errs;
+    vector_tile::Tile_Feature feature;
+    error_set_T errs;
 
-    value.SerializeToString(&buffer);
+    feature.SerializeToString(&buffer);
     protozero::pbf_reader pbf_feature(buffer);
 
     feature_is_valid(pbf_feature, errs);
 
     CHECK(errs.empty() == false);
     CHECK(errs.count(mapnik::vector_tile_impl::validity_error::FEATURE_IS_EMPTY) == 1);
+}
+
+TEST_CASE( "invalid geometry without type" )
+{
+    std::string buffer;
+    vector_tile::Tile_Feature feature;
+    error_set_T errs;
+
+    feature.add_geometry(9); // move_to | (1 << 3)
+    feature.add_geometry(protozero::encode_zigzag32(5));
+    feature.add_geometry(protozero::encode_zigzag32(5));
+
+    feature.SerializeToString(&buffer);
+    protozero::pbf_reader pbf_feature(buffer);
+
+    feature_is_valid(pbf_feature, errs);
+
+    CHECK(errs.empty() == false);
+    CHECK(errs.count(mapnik::vector_tile_impl::validity_error::FEATURE_NO_GEOM_TYPE) == 1);
+}
+
+TEST_CASE( "valid raster feature" )
+{
+    std::string buffer;
+    vector_tile::Tile_Feature feature;
+    error_set_T errs;
+
+    feature.set_raster("raster-blaster");
+
+    feature.SerializeToString(&buffer);
+    protozero::pbf_reader pbf_feature(buffer);
+
+    feature_is_valid(pbf_feature, errs);
+
+    CHECK(errs.empty() == true);
+}
+
+TEST_CASE( "valid geometry feature" )
+{
+    std::string buffer;
+    vector_tile::Tile_Feature feature;
+    error_set_T errs;
+
+    feature.set_type(vector_tile::Tile_GeomType::Tile_GeomType_POINT);
+    feature.add_geometry(9); // move_to | (1 << 3)
+    feature.add_geometry(protozero::encode_zigzag32(5));
+    feature.add_geometry(protozero::encode_zigzag32(5));
+
+    feature.SerializeToString(&buffer);
+    protozero::pbf_reader pbf_feature(buffer);
+
+    feature_is_valid(pbf_feature, errs);
+
+    CHECK(errs.empty() == true);
+}
+
+TEST_CASE( "valid geometry feature" )
+{
+    std::string buffer;
+    vector_tile::Tile_Feature feature;
+    error_set_T errs;
+
+    feature.set_type(vector_tile::Tile_GeomType::Tile_GeomType_POINT);
+    feature.add_geometry(9); // move_to | (1 << 3)
+    feature.add_geometry(protozero::encode_zigzag32(5));
+    feature.add_geometry(protozero::encode_zigzag32(5));
+
+    feature.SerializeToString(&buffer);
+    protozero::pbf_reader pbf_feature(buffer);
+
+    feature_is_valid(pbf_feature, errs);
+
+    CHECK(errs.empty() == true);
 }
