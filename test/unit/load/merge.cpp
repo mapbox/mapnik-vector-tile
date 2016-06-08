92619d9ea51f2e5c1fdd11b0edcd2f8c480cc537
Dane Springmeyer <dane@mapbox.com>
2016-06-08 14:46:53 -0700

    stash test work

--- test/unit/load/merge.cpp
+++ test/unit/load/merge.cpp
@@ -93,3 +93,171 @@ TEST_CASE( "merge_from_compressed_buffer - raster" )
         }
     }
 }
+
+TEST_CASE( "merge_from_compressed_buffer - vector with numerous empty layers" )
+{
+    std::map<std::string,std::pair<std::uint8_t,std::uint8_t>> expected_layers;
+    expected_layers.emplace("landuse",std::make_pair(0,0));
+    expected_layers.emplace("waterway",std::make_pair(0,0));
+    expected_layers.emplace("water",std::make_pair(0,0));
+    expected_layers.emplace("aeroway",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("barrier_line",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("building",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("landuse_overlay",std::make_pair(0,0));
+    expected_layers.emplace("tunnel",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("road",std::make_pair(0,0));
+    expected_layers.emplace("bridge",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("admin",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("country_label_line",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("country_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("marine_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("state_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("place_label",std::make_pair(0,0));
+    expected_layers.emplace("water_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("area_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("rail_station_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("airport_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("road_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("waterway_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+    expected_layers.emplace("building_label",std::make_pair(1,mapnik::vector_tile_impl::LAYER_HAS_NO_FEATURES));
+
+    {
+        std::ifstream stream("./test/data/invalid_v2_tile.mvt",std::ios_base::in|std::ios_base::binary);
+        REQUIRE(stream.is_open());
+        std::string buffer(std::istreambuf_iterator<char>(stream.rdbuf()),(std::istreambuf_iterator<char>()));
+
+        REQUIRE(expected_layers.size() == 23);
+        {
+            mapnik::vector_tile_impl::merc_tile tile(0,0,0);
+            // not validating or upgrading
+            mapnik::vector_tile_impl::merge_from_compressed_buffer(tile, buffer.data(), buffer.size());
+            REQUIRE(tile.get_layers().size() == 23);
+            {
+                protozero::pbf_reader tile_msg(tile.get_buffer());
+                std::size_t idx = 0;
+                while (tile_msg.next(mapnik::vector_tile_impl::Tile_Encoding::LAYERS))
+                {
+                    auto layer_data = tile_msg.get_data();
+                    protozero::pbf_reader layer_props_msg(layer_data);
+                    auto layer_info = mapnik::vector_tile_impl::get_layer_name_and_version(layer_props_msg);
+                    std::string const& layer_name = layer_info.first;
+                    auto const& expected = expected_layers[layer_name];
+                    std::uint32_t version = layer_info.second;
+                    CHECK(version == 2);
+                    std::set<mapnik::vector_tile_impl::validity_error> errors;
+                    protozero::pbf_reader layer_valid_msg(layer_data);
+                    layer_is_valid(layer_valid_msg, errors);
+                    INFO(layer_name);
+                    CHECK(errors.size() == expected.first);
+                    if (errors.size() > 0)
+                    {
+                        INFO(layer_name);
+                        CHECK(*errors.begin() == expected.second);
+                    }
+                }
+            }
+        }
+
+        {
+            mapnik::vector_tile_impl::merc_tile tile(0,0,0);
+            // upgrading but not validating
+            mapnik::vector_tile_impl::merge_from_compressed_buffer(tile, buffer.data(), buffer.size(), false, true);
+            REQUIRE(tile.get_layers().size() == 23);
+            {
+                protozero::pbf_reader tile_msg(tile.get_buffer());
+                std::size_t idx = 0;
+                while (tile_msg.next(mapnik::vector_tile_impl::Tile_Encoding::LAYERS))
+                {
+                    auto layer_data = tile_msg.get_data();
+                    protozero::pbf_reader layer_props_msg(layer_data);
+                    auto layer_info = mapnik::vector_tile_impl::get_layer_name_and_version(layer_props_msg);
+                    std::string const& layer_name = layer_info.first;
+                    auto const& expected = expected_layers[layer_name];
+                    std::uint32_t version = layer_info.second;
+                    CHECK(version == 2);
+                    std::set<mapnik::vector_tile_impl::validity_error> errors;
+                    protozero::pbf_reader layer_valid_msg(layer_data);
+                    layer_is_valid(layer_valid_msg, errors);
+                    INFO(layer_name);
+                    CHECK(errors.size() == expected.first);
+                    if (errors.size() > 0)
+                    {
+                        INFO(layer_name);
+                        CHECK(*errors.begin() == expected.second);
+                    }
+                }
+            }
+        }
+    }
+
+    {
+        std::ifstream stream("./test/data/invalid_v1_tile.mvt",std::ios_base::in|std::ios_base::binary);
+        REQUIRE(stream.is_open());
+        std::string buffer(std::istreambuf_iterator<char>(stream.rdbuf()),(std::istreambuf_iterator<char>()));
+
+        REQUIRE(expected_layers.size() == 23);
+        {
+            mapnik::vector_tile_impl::merc_tile tile(0,0,0);
+            // not validating or upgrading
+            mapnik::vector_tile_impl::merge_from_compressed_buffer(tile, buffer.data(), buffer.size());
+            REQUIRE(tile.get_layers().size() == 23);
+            {
+                protozero::pbf_reader tile_msg(tile.get_buffer());
+                std::size_t idx = 0;
+                while (tile_msg.next(mapnik::vector_tile_impl::Tile_Encoding::LAYERS))
+                {
+                    auto layer_data = tile_msg.get_data();
+                    protozero::pbf_reader layer_props_msg(layer_data);
+                    auto layer_info = mapnik::vector_tile_impl::get_layer_name_and_version(layer_props_msg);
+                    std::string const& layer_name = layer_info.first;
+                    auto const& expected = expected_layers[layer_name];
+                    std::uint32_t version = layer_info.second;
+                    CHECK(version == 1);
+                    std::set<mapnik::vector_tile_impl::validity_error> errors;
+                    protozero::pbf_reader layer_valid_msg(layer_data);
+                    layer_is_valid(layer_valid_msg, errors);
+                    INFO(layer_name);
+                    CHECK(errors.size() == expected.first);
+                    if (errors.size() > 0)
+                    {
+                        INFO(layer_name);
+                        CHECK(*errors.begin() == expected.second);
+                    }
+                }
+            }
+        }
+
+        {
+            mapnik::vector_tile_impl::merc_tile tile(0,0,0);
+            // upgrading but not validating
+            mapnik::vector_tile_impl::merge_from_compressed_buffer(tile, buffer.data(), buffer.size(), false, true);
+            REQUIRE(tile.get_layers().size() == 6);
+            {
+                protozero::pbf_reader tile_msg(tile.get_buffer());
+                std::size_t idx = 0;
+                while (tile_msg.next(mapnik::vector_tile_impl::Tile_Encoding::LAYERS))
+                {
+                    auto layer_data = tile_msg.get_data();
+                    protozero::pbf_reader layer_props_msg(layer_data);
+                    auto layer_info = mapnik::vector_tile_impl::get_layer_name_and_version(layer_props_msg);
+                    std::string const& layer_name = layer_info.first;
+                    auto const& expected = expected_layers[layer_name];
+                    std::uint32_t version = layer_info.second;
+                    CHECK(version == 2);
+                    std::set<mapnik::vector_tile_impl::validity_error> errors;
+                    protozero::pbf_reader layer_valid_msg(layer_data);
+                    layer_is_valid(layer_valid_msg, errors);
+                    INFO(layer_name);
+                    CHECK(errors.size() == expected.first);
+                    CHECK(errors.size() == 0);
+                    if (errors.size() > 0)
+                    {
+                        INFO(layer_name);
+                        CHECK(*errors.begin() == expected.second);
+                    }
+                }
+            }
+        }
+
+    }
+}
