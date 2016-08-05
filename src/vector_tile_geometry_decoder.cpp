d992c4771e6b6c20fc9bf30ad44c260f6291207b
Dane Springmeyer <dane@mapbox.com>
2016-08-05 15:01:00 -0700

    fix #217

--- src/vector_tile_geometry_decoder.cpp
+++ src/vector_tile_geometry_decoder.cpp
@@ -22,6 +22,22 @@ template mapnik::geometry::geometry<std::int64_t> decode_geometry<std::int64_t>(
                                                                                 std::int64_t tile_y,
                                                                                 double scale_x, 
                                                                                 double scale_y);
+template mapnik::geometry::geometry<double> decode_geometry<double>(GeometryPBF & paths, 
+                                                                    int32_t geom_type, 
+                                                                    unsigned version,
+                                                                    double tile_x,
+                                                                    double tile_y,
+                                                                    double scale_x, 
+                                                                    double scale_y,
+                                                                    mapnik::box2d<double> const& bbox);
+template mapnik::geometry::geometry<std::int64_t> decode_geometry<std::int64_t>(GeometryPBF & paths, 
+                                                                                int32_t geom_type, 
+                                                                                unsigned version,
+                                                                                std::int64_t tile_x,
+                                                                                std::int64_t tile_y,
+                                                                                double scale_x, 
+                                                                                double scale_y,
+                                                                                mapnik::box2d<double> const& bbox);
 
 } // end ns vector_tile_impl
 
