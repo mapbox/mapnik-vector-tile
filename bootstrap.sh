b987a3a71aa52f65142952205bb1057120660cbd
Dane Springmeyer <dane@mapbox.com>
2016-08-31 17:44:38 -0700

    update mason/deps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MASON_VERSION="b709931"
+MASON_VERSION="new-pkgs"
 
 function setup_mason() {
     if [[ ! -d ./.mason ]]; then
@@ -19,6 +19,8 @@ function install() {
     MASON_PLATFORM_ID=$(mason env MASON_PLATFORM_ID)
     if [[ ! -d ./mason_packages/${MASON_PLATFORM_ID}/${1}/ ]]; then
         mason install $1 $2
+        # the rm here is to workaround https://github.com/mapbox/mason/issues/230
+        rm -f ./mason_packages/.link/mason.ini
         mason link $1 $2
     fi
 }
@@ -26,28 +28,42 @@ function install() {
 ICU_VERSION="55.1"
 
 function install_mason_deps() {
-    install ccache 3.2.4 &
+    FAIL=0
     install mapnik latest &
-    install protobuf 2.6.1 &
-    install freetype 2.6 &
-    install harfbuzz 0.9.41 &
-    wait
-    install jpeg_turbo 1.4.0 &
-    install libpng 1.6.20 &
-    install webp 0.4.2 &
-    install gdal 1.11.2 libgdal &
+    install ccache 3.2.4 &
+    install jpeg_turbo 1.5.0 libjpeg &
+    install libpng 1.6.24 libpng &
+    install libtiff 4.0.6 libtiff &
+    install libpq 9.5.2 &
+    install sqlite 3.14.1 libsqlite3 &
+    install expat 2.2.0 libexpat &
     install icu ${ICU_VERSION} &
-    install proj 4.8.0 &
-    install libtiff 4.0.4beta &
-    wait
+    install proj 4.9.2 libproj &
+    install pixman 0.34.0 libpixman-1 &
+    install cairo 1.14.6 libcairo &
+    install protobuf 2.6.1 &
+    # technically protobuf is not a mapnik core dep, but installing
+    # here by default helps make mapnik-vector-tile builds easier
+    install webp 0.5.1 libwebp &
+    install gdal 2.1.1 libgdal &
     install boost 1.61.0 &
     install boost_libsystem 1.61.0 &
     install boost_libfilesystem 1.61.0 &
     install boost_libprogram_options 1.61.0 &
-    install boost_libregex 1.61.0 &
-    install pixman 0.32.6 &
-    install cairo 1.14.2 &
-    wait
+    install boost_libregex_icu 1.61.0 &
+    # technically boost thread and python are not a core dep, but installing
+    # here by default helps make python-mapnik builds easier
+    install boost_libthread 1.61.0 &
+    install boost_libpython 1.61.0 &
+    install freetype 2.6.5 libfreetype &
+    install harfbuzz 1.3.0 libharfbuzz &
+    for job in $(jobs -p)
+    do
+        wait $job || let "FAIL+=1"
+    done
+    if [[ "$FAIL" != "0" ]]; then
+        exit ${FAIL}
+    fi
 }
 
 function setup_runtime_settings() {
