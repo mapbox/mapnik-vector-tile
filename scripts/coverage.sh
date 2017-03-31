70b4af4c8fa909e6a4b3ea879fefb89122b45e1e
Dane Springmeyer <dane@mapbox.com>
2017-03-31 13:18:56 -0700

    download codecov command

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,5 +4,7 @@ set -e -u
 set -o pipefail
 
 if [[ ${COVERAGE:-false} != false ]]; then
+    curl -S -f https://codecov.io/bash -o codecov
+    chmod +x codecov
     ./codecov -x "llvm-cov gcov" -Z
 fi
