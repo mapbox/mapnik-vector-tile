8966bc5ccae2e2f0e6181532df855887d5d95169
Dane Springmeyer <dane@mapbox.com>
2017-08-14 11:21:56 -0700

    fix unbound variables

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,11 @@
 function setup_runtime_settings() {
     local MASON_LINKED_ABS=$(pwd)/mason_packages/.link
     export PROJ_LIB=${MASON_LINKED_ABS}/share/proj
+    ICU_VERSION=$(ls ${MASON_LINKED_ABS}/share/icu/)
     export ICU_DATA=${MASON_LINKED_ABS}/share/icu/${ICU_VERSION}
     export GDAL_DATA=${MASON_LINKED_ABS}/share/gdal
     if [[ $(uname -s) == 'Darwin' ]]; then
-        export DYLD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${DYLD_LIBRARY_PATH}
+        export DYLD_LIBRARY_PATH=$(pwd)/mason_packages/.link/lib:${DYLD_LIBRARY_PATH:-}
         # OS X > 10.11 blocks DYLD_LIBRARY_PATH so we pass along using a
         # differently named variable
         export MVT_LIBRARY_PATH=${DYLD_LIBRARY_PATH}
