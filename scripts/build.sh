3d6ea56b242fb528b7d7c45874773ff57bad0b9f
Dane Springmeyer <dane@mapbox.com>
2017-09-09 18:47:58 -0700

    Actually fix coverage buildg

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,8 @@ if [[ ${COVERAGE:-false} == true ]]; then
     # test only debug version
     export LDFLAGS="--coverage"
     export CXXFLAGS="--coverage"
+    make debug -j${JOBS:-1} V=1
+    make test-debug
 else
     # test both release and debug
     make release -j${JOBS:-1} V=1
