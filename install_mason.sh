b36e8491ee0968730916aeb1261445ced52afaf8
Blake Thompson <flippmoke@gmail.com>
2018-02-05 17:53:58 -0500

    Update to mapnik 3.0.18

--- install_mason.sh
+++ install_mason.sh
@@ -9,7 +9,7 @@ function install() {
 }
 
 ICU_VERSION="57.1"
-MASON_VERSION="v0.14.2"
+MASON_VERSION="9a3f4d195614bb30d9ca54dd6070739de605cb14"
 
 if [ ! -f ./mason/mason.sh ]; then
     mkdir -p ./mason
@@ -35,14 +35,14 @@ if [[ ${SKIP_MAPNIK_INSTALL:-} != 'YES' ]] && [[ ! -f ./mason_packages/.link/bin
     install cairo 1.14.8
     install webp 0.6.0
     install libgdal 2.1.3
-    install boost 1.63.0
-    install boost_libsystem 1.63.0
-    install boost_libfilesystem 1.63.0
-    install boost_libregex_icu57 1.63.0
+    install boost 1.65.1
+    install boost_libsystem 1.65.1
+    install boost_libfilesystem 1.65.1
+    install boost_libregex_icu57 1.65.1
     install freetype 2.7.1
     install harfbuzz 1.4.2-ft
 
     # mapnik
-    install mapnik 3.0.14
+    install mapnik 3.0.18
 
 fi
